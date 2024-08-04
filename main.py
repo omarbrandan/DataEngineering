ef9b8cd371e71e04c410a5d2d7e8c64ed208dbe6
omarbrandan <141689001+omarbrandan@users.noreply.github.com>
2024-08-04 19:12:41 +0000

    main y req

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-
-import sys
-import os
-
-# Agregar el directorio del proyecto al PYTHONPATH
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 from modules.coingecko import obtener_datos_coingecko
 from modules.redshift import cargar_datos_redshift
 from modules.utils import log_error
