2be3ec0433773e643970cefbd35699113355d13d
omarbrandan <141689001+omarbrandan@users.noreply.github.com>
2024-09-02 23:59:13 +0000

    DAGs3

--- dags/dags_modules/__init__.py
+++ dags/dags_modules/__init__.py
@@ -1,4 +1,3 @@
 from dags_modules.data_extract import extract_data
 from dags_modules.data_load import preparar_y_cargar_datos
 from dags_modules.data_transform import transform_data
-from dags_utils import get_defaultairflow_args
